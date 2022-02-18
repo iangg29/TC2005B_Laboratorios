d2ec16d9f289ee34b0d3ec01c124a9eb162af99a
Ian García <iangarciag@outlook.com>
2022-02-17 23:14:48 -0600

    Minor changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
+app.use('/js', express.static(__dirname + '/dist/assets/js'));
+
 let server = app.listen(PORT, () => {
     console.log("[STATUS] Server running on port ", PORT);
 });
