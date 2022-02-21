502ad642f1822c28ed2c8bd1186c2d10ef3b3019
Ian García <iangarciag@outlook.com>
2022-02-21 12:17:00 -0600

    New updates

---
+++ src/assets/typescript/lab4.ts
@@ -0,0 +1,59 @@
+const generateArray = (num: number): number[] => {
+    let array: number[] = [];
+    for (let i = 1; i <= num; ++i) {
+        array.push(Math.floor(Math.random() * 100));
+    }
+    return array;
+};
+
+const drawTable = (id: string): void => {
+    let userInput: string | null = prompt("Ingresa un número:");
+    if (userInput == null) return;
+    let num: number = parseInt(userInput);
+    if (num <= 0) return;
+    let element: HTMLElement | null = document.getElementById(id);
+    let tableBody: HTMLTableSectionElement | undefined = document.getElementById("ex1Table")?.getElementsByTagName('tbody')[0];
+    for (let i = 1; i <= num; ++i) {
+        let newRow: HTMLTableRowElement | undefined = tableBody?.insertRow();
+        let numCell: HTMLTableCellElement | undefined = newRow?.insertCell();
+        let squaredCell: HTMLTableCellElement | undefined = newRow?.insertCell();
+        let cubedCell: HTMLTableCellElement | undefined = newRow?.insertCell();
+        numCell?.classList.add("px-6");
+        numCell?.classList.add("py-4");
+        numCell?.classList.add("whitespace-nowrap");
+        squaredCell?.classList.add("px-6");
+        squaredCell?.classList.add("py-4");
+        squaredCell?.classList.add("whitespace-nowrap");
+        cubedCell?.classList.add("px-6");
+        cubedCell?.classList.add("py-4");
+        cubedCell?.classList.add("whitespace-nowrap");
+        let numTxt = document.createTextNode(i.toString());
+        let squaredTxt = document.createTextNode((i * i).toString());
+        let cubedTxt = document.createTextNode((i * i * i).toString());
+        numCell?.appendChild(numTxt);
+        squaredCell?.appendChild(squaredTxt);
+        cubedCell?.appendChild(cubedTxt);
+    }
+    element?.classList.toggle('hidden');
+};
+const quiz = (): void => {
+
+};
+const contador = (numArra: number[]): void => {
+
+};
+const promedios = (numArray: number[][]): number[] => {
+    let answer: number[] = [];
+    for (let i = 0; i < numArray.length; ++i) {
+        answer.push(numArray[i].reduce((x: number, y: number) => x + y, 0) / numArray[i].length);
+    }
+    return answer;
+};
+const inverso = (num: number): number => {
+    return -1;
+};
+
+drawTable("exercise1");
+quiz();
+contador([1, 2, 3]);
+promedios([generateArray(6), generateArray(10)]);
