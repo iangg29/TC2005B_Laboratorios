28e426f753e5489e6610df7e7c0a01c5f0415034
Ian García <iangarciag@outlook.com>
2022-05-04 22:41:16 -0500

    Implemented lab 10

--- Labs/Lab10/index.js
+++ Labs/Lab10/index.js
@@ -1,38 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const PORT = process.env.PORT || 8989;
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const app = express();
+const server = http.createServer();
+const port = process.env.PORT || 3000;
 
-app.enable('trust proxy');
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(morgan('dev'));
+const indexFile = fs.readFileSync('./public/index.html');
+const cardsFile = fs.readFileSync('./public/cards.html');
 
-const horseRoutes = require('./routes/horses');
-
-app.use((req, res, next) => {
-    next();
+server.on('request', (req, res) => {
+    switch (req.url) {
+        case '/':
+            if (req.method === "GET"){
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(indexFile);
+            } else if (req.method === "POST"){
+                const data = [];
+                req.on('data', (chunk) => {
+                    data.push(chunk);
+                });
+                return req.on('end', () => {
+                    const parsedData = Buffer.concat(data).toString();
+                    console.log(parsedData);
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(parsedData);
+                    return res.end(indexFile);
+                });
+            }
+            break;
+        case '/data':
+            fs.createReadStream('./public/data.json').pipe(res);
+            break;
+        case '/cards':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(cardsFile);
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('<h1>Error 404: Ruta no encontrada.</h1>');
+            break;
+    }
 });
 
-app.get('/', (req, res) => {
-    return res.status(200).json({
-        success: true,
-        message: "Service available."
-    });
-});
-
-app.use('/horses', horseRoutes);
-
-app.get('/alan', (req, res) => {
-    console.log(req.body);
-    return res.status(200).json({
-        success: true,
-        data: req.body
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`[STATUS] Server listening on port: ${PORT}`);
+server.listen(port, 'localhost', () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
